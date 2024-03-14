29341e930ffc56cbf6f4f76630fa721f5e71b85a
Kevin <113167092+Lagergrenk@users.noreply.github.com>
2024-03-14 12:44:37 +0100

    feat: generateHTMLElement function added

--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -1,11 +1,17 @@
+// Porpuse: Contains utility functions to be used in the project.
+
+export function formatPrice(price: number) {
+  return `$${price.toFixed(2)}`;
+}
 export function isStockLow(stock: number) {
   return stock < 10;
 }
 
 export function generateHTMLBuyButton() {
   const btn = document.createElement("button");
-  btn.classList.add("buy-button");
-  btn.textContent = "Buy Now";
+  btn.classList.add("add-to-cart-button");
+  btn.classList.add("btn");
+  btn.textContent = "Add to cart";
   return btn;
 }
 
@@ -16,8 +22,19 @@ export function generateHTMLImage(src: string) {
   return img;
 }
 
-export function generateHTMLParagraph(text: string) {
-  const p = document.createElement("p");
+export function generateHTMLElement(element: string, text: string) {
+  const p = document.createElement(element);
   p.textContent = text;
   return p;
+}
+
+export function capitalizeFirstLetter(word: string) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+export function clearProductsList() {
+  const productContainer = document.querySelector(
+    ".main__product-list"
+  ) as HTMLElement;
+  productContainer.innerHTML = "";
 }
